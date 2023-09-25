d704c6ed0c0351e0efa1a81aa47016f80ce17aa4
EmeEmu <matteo.d.kott@gmail.com>
2023-09-25 18:46:46 +0200

    faster nRMSE computation for large datasets

--- utils/nRMSE.jl
+++ utils/nRMSE.jl
@@ -1,8 +1,11 @@
 function rmse(y_true::Vector, y_pred::Vector)
     return sqrt.(sum((y_true .- y_pred).^2) / length(y_true))
 end
+function rmse(y_true::Base.ReshapedArray, y_pred::Base.ReshapedArray)
+    return sqrt.(sum((y_true .- y_pred).^2) / length(y_true))
+end
 
-function nRMSE(X::AbstractArray, Y::AbstractArray; X_opt::Union{Nothing,AbstractArray}=nothing, Y_opt::Union{Nothing,AbstractArray}=nothing)
+function nRMSE(X::AbstractArray, Y::AbstractArray; X_opt::Union{Nothing,AbstractArray}=nothing, Y_opt::Union{Nothing,AbstractArray}=nothing, max_nb=10^8)
     @assert size(Y) == size(X)
     if !isa(X_opt, Nothing)
         @assert size(X_opt) == size(X)
@@ -11,19 +14,37 @@ function nRMSE(X::AbstractArray, Y::AbstractArray; X_opt::Union{Nothing,Abstract
         @assert size(Y_opt) == size(X)
     end
     
-    X = vec(X)
-    Y = vec(Y)
-    if !isa(X_opt, Nothing)
-        X_opt = vec(X_opt)
+    if length(X) > max_nb
+        n = round(Int,sqrt(max_nb))
+        indsi = randperm(size(X,1))[1:n]
+        indsj = randperm(size(X,1))[1:n]
+        @views X = vec(X[indsi,indsj])
+        @views Y = vec(Y[indsi,indsj])
+        if !isa(X_opt, Nothing)
+            #X_opt = vec(X_opt)
+            @views X_opt = vec(X_opt[indsi,indsj])
+        end
+        if !isa(Y_opt, Nothing)
+            #Y_opt = vec(Y_opt)
+            @views Y_opt = vec(Y_opt[indsi,indsj])
+        end 
+    else
+        X = vec(X)
+        Y = vec(Y)
+        if !isa(X_opt, Nothing)
+            X_opt = vec(X_opt)
+        end
+        if !isa(Y_opt, Nothing)
+            Y_opt = vec(Y_opt)
+        end 
     end
-    if !isa(Y_opt, Nothing)
-        Y_opt = vec(Y_opt)
-    end 
     
     RMSE_ord = rmse(X,Y)
     
-    X_shuf = shuffle(X)
-    Y_shuf = shuffle(Y)
+    #X_shuf = shuffle(X)
+    #Y_shuf = shuffle(Y)
+    X_shuf = X[shuffle(1:length(X))]
+    Y_shuf = Y[shuffle(1:length(Y))]
     RMSE_shuf = rmse(X_shuf, Y_shuf)
     
     if isa(X_opt, Nothing) && isa(Y_opt, Nothing)
