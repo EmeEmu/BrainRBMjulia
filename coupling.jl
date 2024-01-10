a5697d792baaf6373dc5b6d3402fe06e7d43b0bf
EmeEmu <matteo.d.kott@gmail.com>
2024-01-10 11:22:35 +0100

    fixing transpose issue in coupling computation

--- coupling.jl
+++ coupling.jl
@@ -2,7 +2,7 @@ function coupling_approx(rbm::Union{RBM,StandardizedRBM}, v::AbstractArray)
   I = inputs_h_from_v(rbm, v)
   cond_var = var_from_inputs(rbm.hidden, I)
   mean_var = mean(cond_var, dims=2)
-  J = rbm.w' * (rbm.w .* reshape(mean_var, 1, :))
+  J = rbm.w * (rbm.w .* reshape(mean_var, 1, :))'
   J[diagind(J)] .= 0
   return J
 end
